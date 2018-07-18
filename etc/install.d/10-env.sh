cd1291ad6fc81ab16f47f00f5201aaacb7e6a860
Joe Fredette <jfredett@gmail.com>
2018-07-18 10:03:23 -0400

    overwrite, don't add, to env_loader on install.

--- etc/install.d/10-env.sh
+++ etc/install.d/10-env.sh
@@ -2,4 +2,4 @@
 
 mkdir -p "$INSTALL_DIR/var/"
 
-cat "$INSTALL_DIR/lib/templates/env_loader" >> "$INSTALL_DIR/var/env_loader"
+cat "$INSTALL_DIR/lib/templates/env_loader" > "$INSTALL_DIR/var/env_loader"
