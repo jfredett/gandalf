cd9c2a4143c35c3e7efe10803bae4552487259f4
Joe Fredette <jfredett@gmail.com>
2020-05-11 00:33:10 -0400

    [cleanup] whitespace

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 fail_unless_exists() {
   which $1 &> /dev/null || die "missing executable $1" 1
 }
+
 fail_unless() {
   eval $1 || die "failed when executing '$1', ${2:="status $!"}" 2
 }
@@ -25,7 +26,6 @@ execute() {
 
 fail_unless_exists readlink
 fail_unless "readlink -f $0 &>/dev/null" "readlink must support '-f'"
-
 fail_unless_exists dirname
 fail_unless_exists git
 
