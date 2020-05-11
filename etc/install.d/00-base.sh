b20cfd389ee59d46c43448d4178ff83e05968bef
Joe Fredette <jfredett@gmail.com>
2020-05-11 00:30:50 -0400

    [bug] fix write order issue
    ensure shebang is the top line of rc

--- etc/install.d/00-base.sh
+++ etc/install.d/00-base.sh
@@ -5,12 +5,14 @@ source $INSTALL_DIR/lib/info
 
 # write rc files
 for RC_FILE in $RC_FILES ; do
-  echo "export SCRIPT_HOME='$INSTALL_DIR'" > "$HOME/$RC_FILE"
+  echo "#!/usr/bin/env bash" > "$HOME/$RC_FILE"
+  echo "export SCRIPT_HOME='$INSTALL_DIR'" >> "$HOME/$RC_FILE"
   cat "$INSTALL_DIR/lib/templates/rc" >> "$HOME/$RC_FILE"
 done
 
 # write profile files
 for PROFILE_FILE in $PROFILE_FILES ; do
+  echo "#!/usr/bin/env bash" > "$HOME/$PROFILE_FILE"
   echo "export SCRIPT_HOME='$INSTALL_DIR'" > "$HOME/$PROFILE_FILE"
   cat "$INSTALL_DIR/lib/templates/profile" >> "$HOME/$PROFILE_FILE"
 done
